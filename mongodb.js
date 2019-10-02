0faf82263cd3fee02843678d04680f062b6953c6
AmaldevAD <amaldev.ad71868@github.com>
2019-10-02 22:04:27 +0530

    add new collection in db

--- mongodb.js
+++ mongodb.js
@@ -9,6 +9,11 @@ MongoClient.connect(connectionURL,{useNewUrlParser:true},(error,client)=>{
     if(error){
         return console.log("unable to connect to database")
     }
-
-    console.log('Connected correctly')
+    // console.log('Connected correctly')
+   const db= client.db(databaseName)
+   
+   db.collection('users').insertOne({
+       name:"Amaldev",
+       age:27
+   })
 })
